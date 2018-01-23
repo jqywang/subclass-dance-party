3b183a32f81bb65f6c3283d04d96b14e23aac754
Trevor Old <trevor.old@outlook.com>
2018-01-22 18:47:43 -0800

    Added squirtle dance

--- src/init.js
+++ src/init.js
@@ -28,6 +28,7 @@ $(document).ready(function() {
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
+    $('body').append('<img id="squirtle" src="https://uploads.scratch.mit.edu/users/avatars/18632851.png">');
   });
 });
 
